342f39f39dedc098923d1beecf37ca3234f74b65
jencinaSoluto <jonathan.encina@solutoria.cl>
2017-08-08 16:23:45 -0300

    avances

--- backend/views/usuario/index.php
+++ backend/views/usuario/index.php
@@ -63,106 +63,10 @@ $this->params['tittle'] = 'Bienvenido';
     </div>
 
     <div class="col-md-12">
-
-        <!-- BEGIN MODAL -->
-        <div class="modal fade none-border" id="event-modal">
-            <div class="modal-dialog">
-                <div class="modal-content">
-                    <div class="modal-header">
-                        <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-                        <h4 class="modal-title"><strong>Add Event</strong></h4>
-                    </div>
-                    <div class="modal-body"></div>
-                    <div class="modal-footer">
-                        <button type="button" class="btn btn-white waves-effect" data-dismiss="modal">Close</button>
-                        <button type="button" class="btn btn-success save-event waves-effect waves-light">Create event</button>
-                        <button type="button" class="btn btn-danger delete-event waves-effect waves-light" data-dismiss="modal">Delete</button>
-                    </div>
-                </div>
-            </div>
-        </div>
-
-        <!-- Modal Add Category -->
-        <div class="modal fade none-border" id="add-category">
-            <div class="modal-dialog">
-                <div class="modal-content">
-                    <div class="modal-header">
-                        <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-                        <h4 class="modal-title"><strong>Add</strong> a category</h4>
-                    </div>
-                    <div class="modal-body">
-                        <form role="form">
-                            <div class="row">
-                                <div class="col-md-6">
-                                    <label class="control-label">Category Name</label>
-                                    <input class="form-control form-white" placeholder="Enter name" type="text" name="category-name"/>
-                                </div>
-                                <div class="col-md-6">
-                                    <label class="control-label">Choose Category Color</label>
-                                    <select class="form-control form-white" data-placeholder="Choose a color..." name="category-color">
-                                        <option value="success">Success</option>
-                                        <option value="danger">Danger</option>
-                                        <option value="info">Info</option>
-                                        <option value="pink">Pink</option>
-                                        <option value="primary">Primary</option>
-                                        <option value="warning">Warning</option>
-                                        <option value="inverse">Inverse</option>
-                                    </select>
-                                </div>
-                            </div>
-                        </form>
-                    </div>
-                    <div class="modal-footer">
-                        <button type="button" class="btn btn-white waves-effect" data-dismiss="modal">Close</button>
-                        <button type="button" class="btn btn-danger waves-effect waves-light save-category" data-dismiss="modal">Save</button>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <!-- END MODAL -->
-
-        <div class="widget">
-            <div class="widget-body">
-                <div class="row">
-                    <div class="col-md-12 col-sm-12 col-xs-12">
-                        <a href="#" data-toggle="modal" data-target="#add-category" class="btn btn-lg btn-default btn-block waves-effect waves-light">
-                            <i class="fa fa-plus"></i> Create New
-                        </a>
-                        <div id="external-events" class="m-t-20">
-                            <br>
-                            <p>Drag and drop your event or click in the calendar</p>
-                            <div class="external-event bg-primary" data-class="bg-primary" style="position: relative;">
-                                <i class="fa fa-move"></i>My Event One
-                            </div>
-                            <div class="external-event bg-pink" data-class="bg-pink" style="position: relative;">
-                                <i class="fa fa-move"></i>My Event Two
-                            </div>
-                            <div class="external-event bg-info" data-class="bg-info" style="position: relative;">
-                                <i class="fa fa-move"></i>My Event Three
-                            </div>
-                            <div class="external-event bg-purple" data-class="bg-purple" style="position: relative;">
-                                <i class="fa fa-move"></i>My Event Four
-                            </div>
-                        </div>
-
-                        <!-- checkbox -->
-                        <div class="checkbox m-t-40">
-                            <input id="drop-remove" type="checkbox">
-                            <label for="drop-remove">
-                                Remove after drop
-                            </label>
-                        </div>
-
-                    </div>
-                </div>
-            </div>
-        </div>
-
         <div class="card-box">
-            <div id="calendar"></div>
+            <?= $this->render('//calendario/calendario') ?>
         </div>
     </div> <!-- end col -->
-
 </div>
 
 <?php
@@ -174,15 +78,6 @@ $updateUsuarios = \yii\helpers\Json::htmlEncode(Url::to(['modulo/updateusuarios'
 
 $urlGetPost = \yii\helpers\Json::htmlEncode(Url::to(['modulo/getpost']));
 $updatePost = \yii\helpers\Json::htmlEncode(Url::to(['modulo/updatepost']));
-
-$this->registerJsFile(Yii::getAlias('@web') . '/plugins/jquery-ui/jquery-ui.min.js', ['depends' => [\backend\assets\AdminAsset::className()]]);
-$this->registerCssFile(Yii::getAlias('@web') . '/plugins/jquery-ui/jquery-ui.css', ['depends' => [\backend\assets\AdminAsset::className()]]);
-
-$this->registerJsFile(Yii::getAlias('@web') . '/plugins/moment/moment.js', ['depends' => [\backend\assets\AdminAsset::className()]]);
-$this->registerCssFile(Yii::getAlias('@web') . '/plugins/fullcalendar/dist/fullcalendar.css', []);
-$this->registerJsFile(Yii::getAlias('@web') . '/plugins/fullcalendar/dist/fullcalendar.min.js', ['depends' => [\backend\assets\AdminAsset::className()]]);
-$this->registerJsFile(Yii::getAlias('@web') . '/plugins/jquery.fullcalendar.js', ['depends' => [\backend\assets\AdminAsset::className()]]);
-
 
 $this->registerJs(<<<JS
    /* jquery.form.min.js */
@@ -225,8 +120,6 @@ $this->registerJs(<<<JS
         });
         return false;
     }    
-        
-    
 
     $(document).ajaxComplete(function(){
         
@@ -262,9 +155,7 @@ $this->registerJs(<<<JS
                 },complete:function(jqXHR, textStatus){
         
                 }
-
             });
-
             return false;
         });
         
@@ -442,7 +333,6 @@ $this->registerJs(<<<JS
           
 JS
 );
-
 
 //$this->registerJsFile(Yii::getAlias('@web') . '/plugins/custombox/dist/legacy.min.js', ['depends' => [yii\web\JqueryAsset::className()]]);
 //$this->registerCssFile(Yii::getAlias('@web') . '/plugins/custombox/dist/custombox.min.css', ['depends' => [yii\web\JqueryAsset::className()]]);
